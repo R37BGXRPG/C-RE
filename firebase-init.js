c0d8cf5847d2ba99494d40ab9e326473d0540bfd
R37BGXRPG <bitgamerz693@gmail.com>
2025-07-05 13:00:22 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { app, analytics, auth, database, signInWithPopup, GoogleAuthProvider };
